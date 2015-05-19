b85cb14560ed7ad2cac841084762c6aa4a9dead0
Peege151 <Pjsulli2k@gmail.com>
2015-05-19 18:27:59 -0400

    add files

--- Gruntfile.js
+++ Gruntfile.js
@@ -412,6 +412,9 @@ module.exports = function (grunt) {
     grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
     grunt.task.run(['serve:' + target]);
   });
+  // change the tasks in the list to your production tasks
+  grunt.registerTask('heroku',
+      [ 'autoprefixer', 'imagemin']);
 
   grunt.registerTask('test', [
     'clean:server',
