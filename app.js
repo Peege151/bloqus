3d89b37826f38c3125e5a8c92a4b91cfe7ca6a94
Peege151 <Pjsulli2k@gmail.com>
2015-05-19 20:26:14 -0400

    pre delete dist

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 var express = require('express');
+var router = require('express').Router();
+var path = require('path');
+
 var port = process.env.PORT || 3000;
 var app = express();
-app.use(express.static(__dirname + '/public'));
+
+var root = __dirname
+
+
+
+
+app.use(express.static(__dirname + "/bower_components"));
+app.use(express.static(__dirname + "/app"));
+console.log("Most of the stuff: ", __dirname + "/app")
+console.log("Bower stuff:", __dirname + "/bower_components" )
+
+
+app.get("/", function(req, res, next){
+	console.log("In route")
+	res.sendFile(__dirname + "/app/index.html")
+})
 app.listen(port);
--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var router = require('express').Router();
+var path = require('path');
 
 var port = process.env.PORT || 3000;
 var app = express();
+
+var root = __dirname
+var bowerPath = path.join(root, './bower_components/*');
+var stylePath = path.join(root, './styles/*');
+var browserPath = path.join(root, './browser/*');
+var scripts = path.join(root, './scripts/*');
+
+console.log("bowerPath: ", bowerPath)
+app.use(express.static('bower_components'));
+app.use(express.static('views'));
+app.use(express.static('scripts'));
+app.use(express.static('styles'));
+
 
 
 app.get("/", function(req, res, next){
