49b20f7f6c379ce662331561b7d568527a2f4139
Ben Milton <ben.c.milton@gmail.com>
2015-05-21 12:08:56 -0400

    added rules list to navbar

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -1,11 +1,18 @@
-'use strict'
+'use strict';
 angular.module('bloqusApp')
-.controller("NavCtrl", function ($scope, $rootScope, $state){
+.controller("NavCtrl", function ($scope, $rootScope, $state, ngDialog){
     $scope.$watch(function(){
         if($state.current.name == 'main'){
             $scope.hideNav = true;
         } else {
             $scope.hideNav = false;
         }
-    })
+    });
+
+        $scope.showRules = function () {
+            ngDialog.open({
+                template: 'views/modals/rules-modal.html',
+                controller: 'NavCtrl'
+            })
+        }
 });
