1370f177486ed0df5a3fb20ca230d589a315d5a5
Ben Milton <ben.c.milton@gmail.com>
2015-06-06 19:20:17 -0400

    refactoring routes

--- routes.js
+++ routes.js
@@ -1,30 +1,23 @@
 var router = require('express').Router();
 var sqlite3 = require('sqlite3').verbose();
+var Promise = require("bluebird");
+var sqlFunctions = Promise.promisifyAll(require('./sqlfunctions'));
 
 router.post('/user', function (req, res, next) {
-    var db = new sqlite3.Database('test.db');
-    var newUser;
 
-    db.serialize(function () {
+    var newUser = {
+        id: Math.floor((Math.random() + 1) * 100000),
+        first: req.body.first,
+        last: req.body.last,
+        email: req.body.email,
+        password: req.body.password
+    };
 
-        db.run("CREATE TABLE if not exists user_info (id TEXT, first TEXT, last TEXT, email TEXT, password TEXT)");
+    sqlFunctions.createUser(newUser);
 
-        var stmt = db.prepare("INSERT INTO user_info VALUES (?,?,?,?,?)");
-        newUser = {
-            id: Math.floor((Math.random() + 1) * 100000),
-            first: req.body.first,
-            last: req.body.last,
-            email: req.body.email,
-            password: req.body.password
-        };
+    req.session.user = newUser;
+    res.send(newUser);
 
-        stmt.run(newUser.id, newUser.first, newUser.last, newUser.email, newUser.password);
-
-        stmt.finalize();
-        req.session.user = newUser;
-        res.send(newUser);
-        db.close();
-    });
 });
 
 router.get('/user', function (req, res, next) {
@@ -40,30 +33,15 @@ router.delete('/login', function (req, res, next){
 });
 
 router.post('/login', function (req, res, next) {
-    var db = new sqlite3.Database('test.db');
-    var user,
-        email = req.body.email,
-        password = req.body.password,
-        stmt = "SELECT id, first, last, email, password FROM user_info WHERE email='" +  email + "'";
 
-    db.all(stmt, function (err, row) {
-        if (err) return next(err);
-        else if (!row[0] || row[0].password !== password) res.send(401);
+    sqlFunctions.checkUserLogin(req.body, function (user){
+        console.log('user login', user)
+        if (!user) res.send(401);
         else {
-            user = {
-                id: row[0].id,
-                first: row[0].first,
-                last: row[0].last,
-                email: row[0].email
-            };
             req.session.user = user;
-
             res.send(user);
         }
-
     });
-
-    db.close();
 
 });
 
--- sqlfunctions.js
+++ sqlfunctions.js
@@ -1,8 +1,8 @@
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('test.db');
 
 var sqlFunctions = {
     createUser:  function (newUser) {
+        var db = new sqlite3.Database('test.db');
         db.serialize(function () {
 
             db.run("CREATE TABLE if not exists user_info (id TEXT, first TEXT, last TEXT, email TEXT, password TEXT)");
@@ -15,8 +15,33 @@ var sqlFunctions = {
             db.close();
             return newUser;
         })
-    }
+    },
 
+    checkUserLogin: function (loginInfo, cb) {
+        var db = new sqlite3.Database('test.db'),
+            email = loginInfo.email,
+            password = loginInfo.password,
+            stmt = "SELECT id, first, last, email, password FROM user_info WHERE email='" +  email + "'",
+            user;
+
+        db.all(stmt, function (err, row) {
+            console.log('sql row', row)
+            console.log('login error', err)
+            if (err) cb(err);
+            else if (!row[0] || row[0].password !== password) cb(user);
+            else {
+                user = {
+                    id: row[0].id,
+                    first: row[0].first,
+                    last: row[0].last,
+                    email: row[0].email
+                };
+
+                db.close();
+                cb(user);
+            }
+        });
+    }
 };
 
 module.exports = sqlFunctions;
